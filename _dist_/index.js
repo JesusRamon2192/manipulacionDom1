c7c56f87bce084155c454097349ffe697e3ebf69
JesusRamon2192 <JesusRamon2192@users.noreply.github.com>
2022-12-24 01:29:07 +0000

    deploy: 1218abf485e3c73b1b61df988cd62b749d8fe356

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,31 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+console.log('Happy hacking :)-----------')
+
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+//Web API
+//Conectarnos al servidor
+window.fetch(url)
+//Procesar la respuesta y convertirla en JSON
+.then(respuesta => respuesta.json())
+//JSON -> Data -> Renderizar info en browser
+.then(responseJSON => {
+    const todosLosItems = [];
+    responseJSON.data.forEach(item => {
+        //crear imagen
+        const imagen = document.createElement("img");
+        //crear titulo
+        const titulo = document.createElement("h2");
+        //crear precio
+        const price = document.createElement("div");
+        
+        const container = document.createElement("div");
+        container.append(imagen, titulo, price);
+
+        todosLosItems.push(container);
+        document.body.appendChild(container);
+    });
+    document.body.append(...todosLosItems);
+});
