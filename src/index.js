dc795faece9539187dfbb0dce9da405ec8f28542
jesusRamon <jesus.ramon2192@gmail.com>
2022-12-24 01:51:01 -0600

    Usando la API de internacionalizacion de monedas y maquetando el css por tailwindcss

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@
 console.log('Happy hacking :)-----------')
 
 const baseUrl = "https://platzi-avo.vercel.app";
+const appNode = document.querySelector("#app");
+
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat("en-EN", {
+        style: "currency",
+        currency: "USD"
+    }).format(price)
+    return newPrice;
+};
+//Internacionalizacion
+//1 - Formato fechas
+//2 - formato monedas
 
 //Web API
 //Conectarnos al servidor
@@ -16,21 +28,34 @@ window.fetch(`${baseUrl}/api/avo`)
 .then(responseJSON => {
     const todosLosItems = [];
     responseJSON.data.forEach(item => {
+
         //crear imagen
         const imagen = document.createElement("img");
         imagen.src = `${baseUrl}${item.image}`;
+        imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+
         //crear titulo
         const titulo = document.createElement("h2");
         titulo.textContent = item.name;
+        titulo.className = "text-lg";
+
         //crear precio
         const price = document.createElement("div");
-        price.textContent = item.price;
+        price.textContent = formatPrice(item.price);
+        price.className = "text-gray-600"
+
+        //Wrap price and title
+        const priceAndTitle = document.createElement("div");
+        priceAndTitle.className = "text-center md:text-left";
+        priceAndTitle.appendChild(titulo);
+        priceAndTitle.appendChild(price);
         
         const container = document.createElement("div");
-        container.append(imagen, titulo, price);
+        container.className = "md:flex border-4 border-double        border-yellow-500	bg-green-500 rounded-lg p-6 hover:bg-gray-300";
+        container.append(imagen, priceAndTitle);
 
         todosLosItems.push(container);
-        document.body.appendChild(container);
     });
-    document.body.append(...todosLosItems);
+    appNode.append(...todosLosItems);
+    appNode.className = "mt-10 grid grid-cols-2 gap-2"
 });
