65fe5f7a71f59a68b8d53f7b6cb60a8728b91724
JesusRamon2192 <JesusRamon2192@users.noreply.github.com>
2022-12-24 07:51:37 +0000

    deploy: dc795faece9539187dfbb0dce9da405ec8f28542

--- _dist_/index.js
+++ _dist_/index.js
@@ -5,29 +5,57 @@
 
 console.log('Happy hacking :)-----------')
 
-const url = "https://platzi-avo.vercel.app/api/avo";
+const baseUrl = "https://platzi-avo.vercel.app";
+const appNode = document.querySelector("#app");
+
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat("en-EN", {
+        style: "currency",
+        currency: "USD"
+    }).format(price)
+    return newPrice;
+};
+//Internacionalizacion
+//1 - Formato fechas
+//2 - formato monedas
 
 //Web API
 //Conectarnos al servidor
-window.fetch(url)
+window.fetch(`${baseUrl}/api/avo`)
 //Procesar la respuesta y convertirla en JSON
 .then(respuesta => respuesta.json())
 //JSON -> Data -> Renderizar info en browser
 .then(responseJSON => {
     const todosLosItems = [];
     responseJSON.data.forEach(item => {
+
         //crear imagen
         const imagen = document.createElement("img");
+        imagen.src = `${baseUrl}${item.image}`;
+        imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+
         //crear titulo
         const titulo = document.createElement("h2");
+        titulo.textContent = item.name;
+        titulo.className = "text-lg";
+
         //crear precio
         const price = document.createElement("div");
+        price.textContent = formatPrice(item.price);
+        price.className = "text-gray-600"
+
+        //Wrap price and title
+        const priceAndTitle = document.createElement("div");
+        priceAndTitle.className = "text-center md:text-left";
+        priceAndTitle.appendChild(titulo);
+        priceAndTitle.appendChild(price);
         
         const container = document.createElement("div");
-        container.append(imagen, titulo, price);
+        container.className = "md:flex border-4 border-double        border-yellow-500	bg-green-500 rounded-lg p-6 hover:bg-gray-300";
+        container.append(imagen, priceAndTitle);
 
         todosLosItems.push(container);
-        document.body.appendChild(container);
     });
-    document.body.append(...todosLosItems);
+    appNode.append(...todosLosItems);
+    appNode.className = "mt-10 grid grid-cols-2 gap-2"
 });
